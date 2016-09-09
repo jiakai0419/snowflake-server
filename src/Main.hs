bdf575c91ca31e17207829db3694cd36766863c6
jiakai0419 <jiakai0419@gmail.com>
2016-09-09 21:35:00 +0800

    clarify code

--- src/Main.hs
+++ src/Main.hs
@@ -20,21 +20,11 @@ import           Control.Concurrent.MVar (MVar, newMVar, putMVar, takeMVar, read
 
 type Pool = Map.Map Int64 (MVar IdWorker)
 
-workerIds = [0..2^(_workerIdBits defaultConf)-1]
+_N = 2^(_workerIdBits defaultConf)
 
-main :: IO ()
-main = do
-  pool <- Map.fromList . zip workerIds <$> mapM (\x -> newMVar $ IdWorker 0 x 0 0 defaultConf) workerIds
-  quickHttpServe $ site pool
-
-site :: Pool -> Snap ()
-site pool = ifTop (writeBS "Hello Snowflake") <|> route [ ("get", getHandler pool) ]
-
-snapshot :: Pool -> IO [(Int64, IdWorker)]
-snapshot = mapM snapshot' . Map.toList
-  where snapshot' (idx, mwk) = do
-          wk <- readMVar mwk
-          return (idx, wk)
+initPool :: IO Pool
+initPool = Map.fromList . zip workerIds <$> mapM (\x -> newMVar $ IdWorker 0 x 0 0 defaultConf) workerIds
+  where workerIds = [0.._N-1]
 
 takeIdWorker :: Pool -> Int64 -> IO IdWorker
 takeIdWorker pool idx = takeMVar . fromJust . Map.lookup idx $ pool
@@ -42,27 +32,25 @@ takeIdWorker pool idx = takeMVar . fromJust . Map.lookup idx $ pool
 putIdWorker :: Pool -> Int64 -> IdWorker -> IO ()
 putIdWorker pool idx worker = let mwk = fromJust . Map.lookup idx $ pool in putMVar mwk worker
 
+dispatchAndGen :: Pool -> Int -> IO [Int64]
+dispatchAndGen pool n = do
+  seed <- ((`mod` _N) . abs . fst . random) <$> newStdGen
+  worker <- takeIdWorker pool seed
+  (ids, newWorker) <- nexts worker n
+  putIdWorker pool seed newWorker
+  return ids
+
+main :: IO ()
+main = do
+  pool <- initPool
+  quickHttpServe $ site pool
+
+site :: Pool -> Snap ()
+site pool = ifTop (writeBS "Hello Snowflake") <|> route [ ("get", getHandler pool) ]
+
 getHandler :: Pool -> Snap ()
 getHandler pool = do
-  poolS <- liftIO $ snapshot pool
-  writeBS $ B8.pack . (<> "\n") . show $ poolS
-
   n <- (fst . fromJust . B8.readInt . head . fromJust . Map.lookup "n" . rqParams) <$> getRequest
-  writeBS $ B8.pack . (<> "\n") . show $ n
-
-  (seed :: Int64) <- liftIO $ ((`mod` 2^(_workerIdBits defaultConf)) . abs . fst . random) <$> newStdGen
-  writeBS $ B8.pack . (<> "\n") . show $ seed
-
-  worker <- liftIO $ takeIdWorker pool seed
-  writeBS $ B8.pack . (<> "\n") . show $ worker
-
-  (ids, newWorker) <- liftIO $ nexts worker n
-  writeBS $ B8.pack . (<> "\n") . show $ ids
-  writeBS $ B8.pack . (<> "\n") . show $ newWorker
-
-  liftIO $ putIdWorker pool seed newWorker
-
-  poolS <- liftIO $ snapshot pool
-  writeBS $ B8.pack . (<> "\n") . show $ poolS
-
+  ids <- liftIO $ dispatchAndGen pool n
+  writeBS . B8.pack . show $ ids
 
